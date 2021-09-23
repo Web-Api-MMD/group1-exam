9d1862447efb19599766a3df8b7c6032295f6e33
Scott Lind <scttlnd@gmail.com>
2021-09-23 10:16:59 +0200

    Work on the frontend

--- frontend/assets/script/script.js
+++ frontend/assets/script/script.js
@@ -8,56 +8,63 @@ const outputDiv = document.querySelector('#outputDiv');
 const APIaddress = 'http://localhost:2090';
 
 // log in
-loginBtn.addEventListener('click', (e) => {
-    if(userEmail.value && userPassword.value) {
-        const payload = {
-            userEmail: userEmail.value,
-            userPassword: userPassword.value
+if(loginBtn) {
+    loginBtn.addEventListener('click', (e) => {
+        if(userEmail.value && userPassword.value) {
+            const payload = {
+                userEmail: userEmail.value,
+                userPassword: userPassword.value
+            }
+    
+            const fetchOptions = {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(payload)
+            }
+    
+            fetch(APIaddress + '/api/accounts/login', fetchOptions)
+            .then(response => {
+                const token = response.headers.get('cn-authenticate-token');
+                localStorage.setItem('cn-authenticate-token', token); 
+                console.log(token);
+    
+                return response.json();
+            })
+            .then(data => {
+                console.log(data);
+                localStorage.setItem('accountInfo', JSON.stringify(data));
+    
+                //  --- sendes til anden side når logget ind 
+                window.location.href = "./discoverIntro.html";
+            })
+            .catch(error => {
+                alert('There was an error. Wrong username or password.');
+            })
+    
+        } else {
+            alert('Please enter user email and password');
         }
-
-        const fetchOptions = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(payload)
-        }
-
-        fetch(APIaddress + '/api/accounts/login', fetchOptions)
-        .then(response => {
-            const token = response.headers.get('cn-authenticate-token');
-            localStorage.setItem('cn-authenticate-token', token); 
-            console.log(token);
-
-            return response.json();
-        })
-        .then(data => {
-            console.log(data);
-            localStorage.setItem('accountInfo', JSON.stringify(data));
-
-            //  --- sendes til anden side når logget ind 
-            window.location.href = "./discoverIntro.html"
-        })
-        .catch(error => {
-            alert('There was an error. Wrong username or password.');
-        })
-
-    } else {
-        alert('Please enter user email and password');
-    }
-
-});
+    
+    });
+}
 
 // // log out
-// logoutBtn.addEventListener('click', (e) => {
-//     window.localStorage.removeItem('x-authenticate-token');
-//     window.localStorage.removeItem('accountInfo');
+if (logoutBtn) {
+    logoutBtn.addEventListener('click', (e) => {
+        e.preventDefault(); // no refresh - else no redirect
+        window.localStorage.removeItem('cn-authenticate-token');
+        window.localStorage.removeItem('accountInfo');
+    
+        console.log('Account logged out yo');
+        window.location.href = "/frontend/index.html";
 
-//     console.log('Account logged out yo');
-
-//     loginDiv.classList.toggle('hidden');
-//     logoutDiv.classList.toggle('hidden');
-// });
+    
+        // loginDiv.classList.toggle('hidden');
+        // logoutDiv.classList.toggle('hidden');
+    });
+}
 
 // on page load
 window.addEventListener('load', (e) => {
@@ -73,36 +80,39 @@ window.addEventListener('load', (e) => {
     console.log(fetchOptions.headers);
 
     // part to render notes
-    fetchOptions.method = 'GET';
-    fetch(APIaddress + '/api/notes', fetchOptions)
-        .then(response => {
-            return response.json()
-        })
-        .then(data => {
-            for (let i = 0; i < data.length; i++) {
-                let htmlOutput = `
-                <section class="category">
-                    <h3>Notes on CSS</h3>
-                    <a href="./discoverCategory.html">See more</a>
-                </section>
-                <article class="description">
-                    <p>Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore </p>
-                </article>
-                <div class="notes">
-                <article id="noteContent">
-                    <p>${data[i].noteContent}</p>
-                    <h4>${data[i].noteName}</h4>
-                </article>
-                </div>
-                `;
-
-                outputDiv.innerHTML += htmlOutput;
-            }
-
-        })
-        .catch(error => {
-            console.log(error);
-        });
+    if (outputDiv) {
+        console.log(token);
+        fetchOptions.method = 'GET';
+        fetch(APIaddress + '/api/notes', fetchOptions)
+            .then(response => {
+                return response.json()
+            })
+            .then(data => {
+                for (let i = 0; i < data.length; i++) {
+                    let htmlOutput = `
+                    <section class="category">
+                        <h3>Notes on CSS</h3>
+                        <a href="./discoverCategory.html">See more</a>
+                    </section>
+                    <article class="description">
+                        <p>Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore </p>
+                    </article>
+                    <div class="notes">
+                    <article id="noteContent">
+                        <p>${data[i].noteContent}</p>
+                        <h4>${data[i].noteName}</h4>
+                    </article>
+                    </div>
+                    `;
+    
+                    outputDiv.innerHTML += htmlOutput;
+                }
+    
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
 
 
 });
